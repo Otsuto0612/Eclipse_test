fafa11ea041d22a53bc436bddb37b83bdda06824
Otsuto Hyuga <h.otsuto.sys19@morijyobi.ac.jp>
2020-06-10 10:14:29 +0900

    git add .& git commit -m "first commit"
    Signed-off-by: Otsuto Hyuga <h.otsuto.sys19@morijyobi.ac.jp>

---
+++ src/SampleServlet.java
@@ -0,0 +1,37 @@
+
+
+import java.io.IOException;
+
+
+
+/**
+ * Servlet implementation class SampleServlet
+ */
+@WebServlet("/SampleServlet") //←これがURLパターンとなる。
+public class SampleServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public SampleServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
